fc9c1e89175504e3ebf4fa4b3da892a8be0efe0c
Nam Doan <southeast.samstation11@gmail.com>
2025-06-04 21:00:03 +0200

    Erster Commit: Python gestartet

---
+++ hello_Python/hello_comment.py
@@ -0,0 +1,13 @@
+"""
+# 1. Frage den Namen des Nutzers ab
+name = input("Wie heißt du?")
+
+# 2. Begrüße den Nutzer
+print ("hello, " )
+
+# 3. Zeige den Namen
+print(name)
+"""
+
+hello = "Sam"
+print("hello", hello)
---
+++ hello_Python/test.py
@@ -0,0 +1,29 @@
+#x = 9
+#y = 2
+#result = x // y
+#print(result)
+
+# wert = "Sam"
+# x = [4, True, "hi"]
+# x.insert(0, wert)
+# print(x)
+
+# list und tuple testen
+"""
+liste = [10, 20, 30]
+tupel = (10, 20, 30)
+
+liste[0] = 99
+tupel[0] = 99
+
+print("Liste:", liste)
+print("Tupel:", tupel) # Tupel sind unveränderlich, daher wird ein Fehler auftreten
+"""
+
+# Position speichern
+position = (100, 200)
+print("Position x:", position[0])
+print("Position y:", position[1])
+
+# Versuch, neue Position zu setzen
+position[0] = 150 # Dies wird einen Fehler verursachen, da Tupel unveränderlich sind

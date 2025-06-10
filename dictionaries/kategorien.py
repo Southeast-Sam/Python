7ce1013d66a488c8f0891596dc2218eee27522ad
Nam Doan <southeast.samstation11@gmail.com>
2025-06-10 20:26:50 +0200

    Add nested dict categories exercise

---
+++ dictionaries/kategorien.py
@@ -0,0 +1,22 @@
+# Kategorisiertes Layout
+kategorien = {
+    "obst": ["Apfel", "Banane", "Orange"],
+    "gemüse": ["Karotte", "Gurke", "Tomate"]
+}
+
+# Zuweisung eine neue Kategorie hinzufügen
+kategorien["getränke"] = ["Wasser", "Tee", "Saft"]
+
+for kategorie, items in kategorien.items():
+    print(f"{kategorie.capitalize()}:")
+    for item in items:
+        print(" -", item)
+    print() # Leerzeile
+
+# den Nutzer nach ein Item abfragen
+auswahl = input("Welche Kategorie möchtest du anzeigen? ")
+if auswahl in kategorien:
+    for item in kategorien[auswahl]:
+        print(item)
+else:
+    print("Kategorie nicht gefunden.")
